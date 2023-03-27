a9a9786cdbfd7af8ffaa83f490360405c87be374
AbdElRahman Atef ElSayed <66162676+Abdelrahman-Atef-Elsayed@users.noreply.github.com>
2023-03-27 23:45:18 +0200

    Add files via upload
    Inserting the input code testing.

---
+++ ANTLR-Project/Input-Code.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+        int y;
+        if (x == 0) {
+            y = 10;
+        } else {
+            if (true) {
+                x++;
+            }
+            y = 1;
+        }
+        if (true) {
+            y++;
+        }
+        int a [] = {1, 2, 3};
+        for (int i = 0; i < 6; i++){
+
+        }
+        if (true) {
+            x--;
+        }
+    }
+}
